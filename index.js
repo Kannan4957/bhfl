221d0f14e1ca12c6481d91b814afcc5bcf4c0a07
Kannan4957 <112773766+Kannan4957@users.noreply.github.com>
2025-02-21 23:47:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express=require('express');
 const app=express();
-const cors = require("cors");
-
 const port = process.env.PORT || 3000;
-app.use(cors());
+
 app.use(express.json());
 
 let num=[];
